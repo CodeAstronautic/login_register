7ae6676e75720c8a583c58546e6ef7a82d6e9b3b
Pooja mishra <mishrapooja8128@gmail.com>
2020-03-29 08:27:53 +0530

    This all files in app

--- urls.py
+++ urls.py
@@ -1,28 +1,19 @@
-"""project URL Configuration
+from django.conf.urls import url
+#from app import views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import views 
+from django.contrib.auth import views as v
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url,include
-from app import views
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^$',views.index,name='index'),
-    url(r'^special/',views.special,name='special'),
-    url(r'^app/',include('app.urls')),
-    url(r'^logout/$', views.user_logout, name='logout'),
+# SET THE NAMESPACE!
+app_name = 'app'
+# Be careful setting the name to just /login use userlogin instead!
+urlpatterns=[
+    url(r'^register/$',auth_views.register,name='register'),
+    url(r'^user_login/$',auth_views.user_login,name='user_login'),
 
-    url('^', include('django.contrib.auth.urls')),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
